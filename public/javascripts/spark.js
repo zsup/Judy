b55c5274ce4b6d892a1dc3a29360b4629ad4fab7
Zach Supalla <zsupalla@gmail.com>
2012-10-23 12:09:25 -0500

    Formatting goodness.

--- public/javascripts/spark.js
+++ public/javascripts/spark.js
@@ -2,6 +2,8 @@ function slabTextHeadlines() {
   $('#slabtext1').fitText(.81);
   $('#slabtext2').fitText(.35);
   $('#slabtext3').fitText(1.33);
+  $('#slabtext4').fitText(1);
+  $('#slabtext5').fitText(.35);
 };
 
 $(document).ready(function() {
