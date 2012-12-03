d31593d9bf414ce51d981a4ecd19bfb4f65e8f73
Zachary Crockett <towynlin@gmail.com>
2012-12-03 02:30:22 -0600

    Subscribe to FB auth.login event

--- public/javascripts/christmas.js
+++ public/javascripts/christmas.js
@@ -1,9 +1,7 @@
-function testAPI() {
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    console.log('Good to see you, ' + response.name + '.');
+function showButtonsIfConnected(response) {
+  if (response.status === 'connected') {
     $('#xmas-buttons').show();
-  });
+  }
 }
 
 window.fbAsyncInit = function() {
@@ -14,24 +12,15 @@ window.fbAsyncInit = function() {
     cookie     : true, // enable cookies to allow the server to access the session
     xfbml      : true  // parse XFBML
   });
-
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      // connected
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // not_authorized
-    } else {
-      // not_logged_in
-    }
-   });
+  FB.Event.subscribe('auth.login', showButtonsIfConnected);
+  FB.getLoginStatus(showButtonsIfConnected);
  };
 
 // Load the SDK Asynchronously
 (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
- }(document));
+  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  ref.parentNode.insertBefore(js, ref);
+}(document));
